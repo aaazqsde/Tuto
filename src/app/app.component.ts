87b3de5dd2e49322a30b0171e16ae5e3d245e004
Hanison <lenoir.guillaume@yahoo.fr>
2017-03-07 15:06:45 +0100

    commitultimateV36

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Test} from './testgit';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'app works! Yeaaaaaaaaaaaaaaaaaaah';
+  test = Test;
+  
 }
